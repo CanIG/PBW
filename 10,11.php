5159f0e324ba11421a943e3bc7b6d78c76ecffb6
CanIG <106538333+CanIG@users.noreply.github.com>
2022-05-30 22:19:56 +0530

    Add files via upload

---
+++ 10,11.php
@@ -0,0 +1,45 @@
+<html>
+<body>
+<form>
+practical 10<br>
+<label><h2>Addmission form</h2></label><br>
+<label>First Name: <input type="text"></label><br><br>
+<label>Last Name: <input type="text"></label><br><br>
+<label>Gender : 
+<input type="radio" name="gender" id="male"> 
+<label for="male">Male</label>
+<input type="radio" name="gender" id="female"> 
+<label for="female">Female</label><br><br>
+<label>Select language :
+<label> <input type="checkbox"> :Marathi</label>
+<label> <input type="checkbox"> :English</label>
+<label> <input type="checkbox"> :Hindi</label><br><br>  
+<input type="submit"><br><br>
+</form>
+
+<form>
+practical 11<br>
+<input type ="hidden" id= "myfile" value= "1234">
+Name :<input type ="text"><br><br>
+Favorite language: 
+<SELECT NAME="language">
+ <OPTION VALUE="c">C
+ <OPTION VALUE="c++">C++
+ <OPTION VALUE="java" SELECTED>Java
+ <OPTION VALUE="lisp">Lisp
+ <OPTION VALUE="perl">Perl
+ <OPTION VALUE="smalltalk">Smalltalk
+</SELECT><br><br>
+Languages you know:<BR>
+<SELECT NAME="language" MULTIPLE>
+ <OPTION VALUE="c">C
+ <OPTION VALUE="c++">C++
+ <OPTION VALUE="java" SELECTED>Java
+ <OPTION VALUE="lisp">Lisp
+ <OPTION VALUE="perl" SELECTED>Perl
+ <OPTION VALUE="smalltalk">Smalltalk
+</SELECT><br><br>
+<input type="submit"><br><br>
+</form>
+</body>
+</html>
