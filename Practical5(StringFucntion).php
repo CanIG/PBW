e5a28a1aa4d7a1f19992ab575096a637750f17d2
CanIG <106538333+CanIG@users.noreply.github.com>
2022-05-30 20:52:46 +0530

    Add files via upload

--- Practical5(StringFucntion).php
+++ Practical5(StringFucntion).php
@@ -8,11 +8,11 @@
 <?php
 echo (strlen("Kumud Patil")."<br>");
 
-$day = "Hello World Programmer""<br>";
-$count = str_word_count($day)
+$day = "Hello World Programmer"."<br>";
+$count = str_word_count($day);
 echo($count);
-echo "<br>"
-echo ("Using str_raplace".str_replace("world","peter","Hello","world")."<br>");
+echo "<br>";
+echo("Using str_replace() :".str_replace("world","everyone","hello world")."<br>");
 echo ("Using str_repeat".str_repeat("WOW",4)."<br>");
 echo ("Using strtolower".strtolower("Kumud Patil")."<br>");
 echo ("Using sttrtoupper".strtoupper("Kumud Patil")."<br>");
